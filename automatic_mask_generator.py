3356a3b875551b55aea2a1aa4fbc0706d49e48c0
rahulrao011 <rahul.seth.rao@gmail.com>
2023-05-10 10:46:06 -0700

    fixed the main file

--- automatic_mask_generator.py
+++ automatic_mask_generator.py
@@ -14,7 +14,7 @@ SCALE_PATH = IMG_NAME + '_Scale' + EXT
 
 
 
-# Get all images
+# Read all images
 img = cv2.imread(IMG_PATH)
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 #scale = cv2.imread(SCALE_PATH, cv2.IMREAD_GRAYSCALE)
@@ -23,7 +23,7 @@ scale = cv2.cvtColor(scale, cv2.COLOR_BGR2RGB)
 
 
 
-# Get model
+# Get SAM
 sam_checkpoint = 'sam_vit_h_4b8939.pth'
 model_type = 'vit_h'
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -43,11 +43,11 @@ mask_generator = SamAutomaticMaskGenerator(
 
 
 
-# Generate masks for images
-masks = mask_generator.generate(img)
+# Generate masks for image
+img_masks = mask_generator.generate(img)
 '''
-masks[i] = {
-    'segmentation': bool np array same shape as img with each each entry either True or False (T if that ppx is in the mask and F if it is not),
+img_masks[i] = {
+    'segmentation': bool np array with same shape as img an entry is True if that ppx is in the mask and False if it is not
     'area': area of mask in ppxs (equivalent to summing over entire array (T is 1 and F is 0)),
     'bbox': boundary box of the mask in (x, y, width, height) format,
     'predicted_iou': the model's own prediction for the quality of the mask,
@@ -59,33 +59,35 @@ masks[i] = {
 
 
 
-# Get scale
+# Get mask that corresponds to center pixel in scale image
 predictor = SamPredictor(sam)
 predictor.set_image(scale)
-scale_center = np.array([[scale.shape[0] // 2, scale.shape[1] // 2]])
+scale_center = np.array([[scale.shape[1] // 2, scale.shape[0] // 2]])
 in_label = np.array([1])
 
 
 
 # Generate mask for scale
-mask, scores, logits = predictor.predict(
+scale_mask, scores, logits = predictor.predict(
     point_coords=scale_center,
     point_labels=in_label,
     multimask_output=False,
 )
-# mask is a bool np array same shape as img with each each entry either True or False (T if that ppx is in the mask and F if it is not)
+scale_mask = np.squeeze(scale_mask) # takes the 1 dim off of scale_mask (scale_mask.shape: (1,x,y) --> (x,y))
+# scale_mask is a bool np array with same shape as img an entry is True if that ppx is in the mask and False if it is not
 
 
 
 # Get pixels per millimeter
-ppx_per_mm = np.sum(mask, axis=1)
+ppx_per_mm = np.sum(scale_mask, axis=1)
 ppx_per_mm = ppx_per_mm[ppx_per_mm > 0]
 ppx_per_mm = np.mean(ppx_per_mm)
+print(f'Pixels per millimeter: {ppx_per_mm}')
 
 
 
 # Get area in square milimeters
-for mask in masks:
+for mask in img_masks:
     area_ppx = mask.pop('area')
     mask['area_ppx'] = area_ppx
     mask['area_mm^2'] = area_ppx / ppx_per_mm ** 2
